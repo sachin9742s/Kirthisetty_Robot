75f30454ad1acd131f219c988e79e9c1ce76d9a9
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-19 18:51:14 +0530

    Update Translation.py

--- DonLee_Robot/Translation.py
+++ DonLee_Robot/Translation.py
@@ -5,45 +5,61 @@
 class Translation(object):
     
     START_TEXT = """<b>👋𝙷𝚎𝚕𝚕𝚘 {}!!</b>
-<b>𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂  [𝐊𝐢𝐫𝐭𝐡𝐢 𝐒𝐡𝐞𝐭𝐭𝐲 𝐑𝐨𝐁𝐨𝐭](https://t.me/Kirthisetty_RoBot)  𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 𝙸𝙽 𝙼𝚈 𝙶𝚁𝙾𝚄𝙿 𝙾𝙽𝙻𝚈,</b>
 
-𝙸𝚃'𝚂 𝚅𝙴𝚁𝚈 𝙴𝙰𝚂𝚈. 𝙹𝚄𝚂𝚃 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚂𝙴𝙰𝚁𝙲𝙷 𝙼𝙾𝚅𝙸𝙴 𝙸𝙽 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙱𝙾𝚃 𝙸 𝚆𝙴𝙻𝙻 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 𝚃𝙷𝙴𝚁𝙴... 
+<b>𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂  <b><i><a href="https://t.me/Kirthisetty_RoBot">𝐊𝐢𝐫𝐭𝐡𝐢 𝐒𝐡𝐞𝐭𝐭𝐲 𝐑𝐨𝐁𝐨𝐭</a></i></b>  𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 𝙸𝙽 𝙼𝚈 𝙶𝚁𝙾𝚄𝙿 𝙾𝙽𝙻𝚈,</b>
 
-𝚃𝚑𝚊𝚗𝚔 𝚈𝚘𝚞 𝙵𝚘𝚛 𝚁𝚎𝚚𝚞𝚎𝚜𝚝 𝙼𝚘𝚟𝚒𝚎𝚜 𝙰𝚗𝚢 𝙿𝚛𝚘𝚋𝚕𝚎𝚖 𝙲𝚘𝚗𝚝𝚊𝚌𝚝 𝙼𝚢 𝙱𝚘𝚜𝚜 𝙰𝚗𝚍 𝚁𝚎𝚚𝚞𝚎𝚜𝚝...😝
+<b>𝙸𝚃'𝚂 𝚅𝙴𝚁𝚈 𝙴𝙰𝚂𝚈. 𝙹𝚄𝚂𝚃 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚂𝙴𝙰𝚁𝙲𝙷 𝙼𝙾𝚅𝙸𝙴 𝙸𝙽 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙱𝙾𝚃 𝙸 𝚆𝙴𝙻𝙻 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 𝚃𝙷𝙴𝚁𝙴...</b>
 
-𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢 𝙼𝚢 𝙱𝚘𝚜𝚜 ☛[𝑺𝒂𝒄𝒉𝒊𝒏 𝑺](https://t.me/sachin_official_admin)
+<b>𝚃𝚑𝚊𝚗𝚔 𝚈𝚘𝚞 𝙵𝚘𝚛 𝚁𝚎𝚚𝚞𝚎𝚜𝚝 𝙼𝚘𝚟𝚒𝚎𝚜 𝙰𝚗𝚢 𝙿𝚛𝚘𝚋𝚕𝚎𝚖 𝙲𝚘𝚗𝚝𝚊𝚌𝚝 𝙼𝚢 𝙱𝚘𝚜𝚜 𝙰𝚗𝚍 𝚁𝚎𝚚𝚞𝚎𝚜𝚝...😝</b>
+
+𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢 𝙼𝚢 𝙱𝚘𝚜𝚜 ☛ <b><i><a href="https://t.me/sachin_official_admin">Sachin S</a></i></b>
 
 <b>𝙿𝚛𝚎𝚜𝚜 /help 𝚃𝚘 𝙺𝚗𝚘𝚠 𝙰𝚋𝚘𝚞𝚝 𝙽𝚘𝚝 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚂𝚎𝚎 𝚃𝚑𝚎 𝙽𝚘𝚝𝚒𝚎𝚜🤪</b>"""    
    
     HELP_TEXT = """
+<b><i><u>How To Use Me!?</u></i></b>
+
+<i>
 ○  it's Note Complicated...🤓
 ○ Available Commands
-     
+</i>
+   
+<i>  
  Basic Commads
 /start :- check am Alive
+</i>
 
-Bot Owner Only
-/add chat_id Replay Any Message or Media</b>
-/settings -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
-/del chat_id Delete File From Database</b>
-/delall  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
-status Button Will Shows The Stats Of Your Channel</b>
-Filter Types - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
+<b>Bot Owner (Works Only In Groups and Bot) :</b>
+-> <code>/add chat_id</code>
+                OR                  - Replay Any Message or Media
+  <code>/add @Username</code>
+
+-> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+-> <code>/del chat_id</code> 
+                OR                  - Delete File From Database
+   <code>/del @Username</code>
+
+-> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+
+-> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+
+<code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
 ○ Notice 📙:-
 ○Dont Spam Me...🤒
-©️MᴀɪɴᴛᴀɪɴᴇD Bʏ :- [SACHIN S](https://t.me/sachin_official_admin)
+©️MᴀɪɴᴛᴀɪɴᴇD By</b> :- <b><i><a href="https://t.me/sachin_official_admin">Sachin S</a></i></b>
 @KicchaRequest
 """
     
-    ABOUT_TEXT = """<b>➥ Name :  [Allu Arjun RoBot](https://t.me/Allu_ArjunROBOT)
-<b>➥ Creator : [Sachin S](https://t.me/sachin_official_admin)
+    ABOUT_TEXT = """<b>➥ Name<b> :<code> Kirthi Shetty RoBot</code>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/sachin_official_admin">Sachin S</a></i></b>
 <b>➥ Credits : Everyone in this journey
-<b>➥ Language : Python3
-<b>➥ Library : Pyrogram Asyncio 1.13.0
-<b>➥ Server : [Heroku](https://herokuapp.com/)
-<b>➥ Source Code : [👉 Click Here](https://t.me/joinchat/UZCvQynJIZJiOWE1)
-<b>➥ Database : [MongoDB](https://www.mongodb.com/)
+<b>➥ Language</b> : <code>Python3</code>
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ Server</b> : <b><i><a href="https://herokuapp.com/">Heroku</a></i></b>
+<b>➥ Source Code</b> : <i><a href="https://t.me/joinchat/UZCvQynJIZJiOWE1">👉 Click Here</a></i>
+<b>➥ Database</b> : <b><i><a href="https://www.mongodb.com/">MongoDB</a></i></b>
 <b>➥ Build Status : V3.1 [BETA]
-<b>➥ Request Group : [Kiccha Request](https://t.me/KicchaRequest)
-<b>➥ Movie Channel :  [KICCHA REQUEST Channel](https://t.me/GD_FILMCLUB)
+<b>➥ Request Group : []() </b> : <b><i><a href="https://t.me/KicchaRequest">Kiccha Request</a></i></b>
+<b>➥ Movie Channel</b> : <b><i><a href="https://t.me/gd_film">KICCHA REQUEST Channel🇮🇳</a></i></b>
 """
